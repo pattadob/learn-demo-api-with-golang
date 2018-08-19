714d8125d2e858693307efdc340a85538d7def16
pattadob <pattadon.b@gmail.com>
2018-08-19 10:16:29 +0700

    Change config API Port to System Environment

--- api/main.go
+++ api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	handlers "github.com/pattadob/learn-demo-api-with-golang/handlers"
 )
@@ -10,9 +11,15 @@ import (
 func main() {
 	log.Print("Starting the service")
 
+	port := os.Getenv("APIPORT")
+
+	if port == "" {
+		log.Fatal("Post is not set.")
+	}
+
 	router := handlers.Router()
 
 	log.Print("The service is ready to listen and serve.")
-	log.Fatal(http.ListenAndServe(":8000", router))
+	log.Fatal(http.ListenAndServe(":"+port, router))
 
 }
